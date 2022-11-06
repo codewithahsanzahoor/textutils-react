54dc3ec97a97eff1dfc8a9fb3650d9cddb60e52c
Ahsan <ahsanzahoor50@gmail.com>
2022-11-07 00:37:01 +0500

    Hooks Practice Dark Mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
 	return (
 		<>
 			<Navbar title="TextUtils" />
-			<div className="container">
-				<TextForm heading="Type the Text here:" />
+			<div className="container my-3">
+				{/* <TextForm heading="Type the Text here:" /> */}
+				<About />
 			</div>
 		</>
 	);
