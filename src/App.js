7a606669ecbbe7b937657c220d32840362990045
Muhammad Ahsan Zahoor <ahsanzahoor490@gmail.com>
2022-11-11 11:15:34 +0500

    Changes to all files for deployment on github pages site deployed success

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, { useState } from 'react';
 import Alert from './components/Alert';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
 	const [mode, setMode] = useState('light'); //whether dark mode is enabled or not in Navbar and whole page
@@ -45,27 +45,27 @@ function App() {
 
 	return (
 		<>
-			<Router>
+			{/* <Router> */}
 				<Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
 				<Alert alert={alert} />
 
 				<div className="container my-3">
-					<Routes>
-						<Route
-							exact
-							path="/"
-							element={
+					{/* <Routes> */}
+						{/* <Route */}
+							{/* exact */}
+							{/* path="/" */}
+							{/* element={ */}
 								<TextForm
 									heading="Type the Text here:"
 									mode={mode}
 									showAlert={changeAlert}
 								/>
-							}
-						/>
-						<Route exact path="/about" element={<About />} />
-					</Routes>
+							{/* } */}
+						{/* /> */}
+						{/* <Route exact path="/about" element={<About />} /> */}
+					{/* </Routes> */}
 				</div>
-			</Router>
+			{/* </Router> */}
 		</>
 	);
 }
